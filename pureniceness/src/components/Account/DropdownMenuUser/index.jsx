14a56c5ce9c3f36b113946b50e0c681f516da6b7
MerylCordier <meryl.cordier@oclock.school>
2024-02-08 16:30:30 +0100

    feature(Dropdown-menu-loggedin-user): add-icon-menu-dropdown whith bug hover

--- pureniceness/src/components/Account/DropdownMenuUser/index.jsx
+++ pureniceness/src/components/Account/DropdownMenuUser/index.jsx
@@ -1,8 +1,30 @@
 // import './index.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+import { NavLink } from "react-router-dom";
+
 
 function DropdownMenuUser() {
 
-    return("Contact")
+    return(
+        <div className="dropdown is-hoverable">
+          
+          <div className="dropdown-trigger">
+            <FontAwesomeIcon icon={faUser} aria-haspopup="true" aria-controls="dropdown-menu4"/>
+          </div>
+
+          <div className="dropdown-menu" id="dropdown-menu4" role="menu">
+            <div className="dropdown-content">
+              <div className="dropdown-item">
+                <NavLink to='/info perso'>Info personnelle</NavLink>
+                <NavLink to='/favorites'>Favoris</NavLink>
+                <NavLink to='/signout'>Se déconnecter</NavLink>
+            </div>
+          </div>
+        </div>
+        
+      </div>)
     
 }
 
--- pureniceness/src/components/Header/NavTop/index.jsx
+++ pureniceness/src/components/Header/NavTop/index.jsx
@@ -1,50 +1,37 @@
 import './index.css'
 // import des icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser, faLock, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLock, faEnvelope,faFolderOpen } from '@fortawesome/free-solid-svg-icons';
 
 import { NavLink } from "react-router-dom";
+import DropdownMenuUser from '../../Account/DropdownMenuUser';
 
 function NavTop() {
 
     return(
-      <>
-      <nav className='navbar-top'>
+      
+      <div className='navbar-top'>
 
         <NavLink to='/'>
           <img className='logo'src='../public/KHF BANNER.jpg' />
         </NavLink>
 
-        <NavLink to='/admin'>
+        <NavLink to='/admin' className='nav-icon'>
           <FontAwesomeIcon icon={faLock} />
         </NavLink>
 
-        <NavLink to='/contact'>
+        <NavLink to='/contact' className='nav-icon'>
           <FontAwesomeIcon icon={faEnvelope} />
         </NavLink>
 
-        <NavLink to='/account'>
-          <div className="dropdown is-hoverable">
-            <div className="dropdown-trigger">
-              <FontAwesomeIcon icon={faUser} aria-haspopup="true" aria-controls="dropdown-menu4"/>
-            </div>
-
-            <div className="dropdown-menu" id="dropdown-menu4" role="menu">
-              <div className="dropdown-content">
-                <div className="dropdown-item">
-                  <NavLink to='/info perso'>Info personnelle</NavLink>
-                  <NavLink to='/favorites'>Favoris</NavLink>
-                  <NavLink to='/signout'>Se déconnecter</NavLink>
-                </div>
-              </div>
-            </div>
-            
-          </div>
+        <NavLink to='/account' className='nav-icon'>
+          <FontAwesomeIcon icon={faFolderOpen} />
+          <DropdownMenuUser/>
         </NavLink>
 
-      </nav>
+      </div>
 
-</>
+
 )
     
 }
