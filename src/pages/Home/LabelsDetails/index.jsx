146f3184e25dd75f52b3ccd1ad90caaa000760d4
MerylCordier <meryl.cordier@oclock.school>
2024-02-13 12:23:47 +0100

    feat(labelsDetails): dynamise render

--- src/pages/Home/LabelsDetails/index.jsx
+++ src/pages/Home/LabelsDetails/index.jsx
@@ -2,26 +2,44 @@
 
 
 function LabelsDetails ({labelsDetails}){
+  
+  //TODO: remplacer Label 1 par le vrai nom
+  const crew = labelsDetails.filter((label)=> label.name === "Label 1")
 
-  console.log("Détails:",labelsDetails);
- 
+  const labels = labelsDetails.filter((label)=> label.name !== "Label 1")
+
   // city,country,created_at,description,id,name,updated_at,url_image,year
-  //ex: fetch récupère repoitems, il map et a dataRepos, et re map datarepos pour avoir repoitem avec de div et tout
-  return(
+return(  
     <>
-      <img className='head-image' src='/images/KHF fisheye.jpg'></img>
+      <img className='hero-image' src={ crew[0].url_image }></img>
+      <section className="hero">        
+        <p className="title">
+          {crew[0].name}
+        </p>
+        <p className="subtitle">
+            { crew[0].year }
+            { crew[0].description }
+            { crew[0].country }
+            { crew[0].city }
+        </p>   
+      </section>    
+
       {
-        labelsDetails.map((label) =>
-          
-            <div className='label__card' key={label.id}>
-              <div className='label__card-header'>
-                {console.log(label.name)}
-              </div>
-              <img className='label__card-image' src={label.url_image} alt={label.name}/>
-              <div className='label__card-content'>
-                { label.description }
-              </div>
+
+      labels.map((label) =>
+
+        <div className='label__card' key={label.id}>
+            <div className='label__card-header'>
+              
             </div>
+            <img className='label__card-image' src={label.url_image} alt={label.name}/>
+            <div className='label__card-content'>
+              { label.year }
+              { label.description }
+              { label.country }
+              { label.city }
+            </div>
+          </div>
           
         )
       }
