61658ea3bc146a51e634eb440977ff9d3a7a3ecd
SAURETMathieu <mathieu.sauret@oclock.school>
2024-02-15 13:09:51 +0100

    feat(admin-index): export all the admin components with index file

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,26 +1,10 @@
-import { useEffect, useContext, useState } from 'react';
-import { Navigate } from 'react-router-dom';
-import checkAdminRole from '../../services/auth/checkAdmin';
-
-import { UserContext } from '../../context/userContext';
-
-function Admin() {
-  const { isAdmin, setIsAdmin } = useContext(UserContext);
-  const [isAdminChecked, setIsAdminChecked] = useState(false);
-
-  useEffect(() => {
-    const adminRole = checkAdminRole();
-    setIsAdmin(adminRole);
-    setIsAdminChecked(true);
-  }, []);
-
-  if (isAdminChecked && !isAdmin) {
-    return <Navigate to="/" replace />;
-  }
-  if (isAdminChecked && isAdmin) {
-    return 'Admin';
-  }
-  return null;
-}
-
-export default Admin;
+export { default as Admin } from './Admin';
+export { default as UserAdmin } from './User/User';
+export { default as ArtistAdmin } from './Artist/Artist';
+export { default as LabelAdmin } from './Label/Label';
+export { default as AlbumAdmin } from './Album/Album';
+export { default as TrackAdmin } from './Track/Track';
+export { default as EventAdmin } from './Event/Event';
+export { default as MediaAdmin } from './Media/Media';
+export { default as MessageAdmin } from './Message/Message';
+export { default as Settings } from './Settings/Settings';
