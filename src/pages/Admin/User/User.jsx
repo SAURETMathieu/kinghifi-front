94eb32b663cf0b486d494096183c534a9614bde0
SAURETMathieu <mathieu.sauret@oclock.school>
2024-02-15 18:04:51 +0100

    feat(admin-user): add admin users page

--- src/pages/Admin/User/User.jsx
+++ src/pages/Admin/User/User.jsx
@@ -1,6 +1,40 @@
+import { useState, useEffect } from 'react';
+import fetchData from '../../../services/api/call.api';
+import AdminTable from '../../../components/Common/Table/AdminTable';
+import AdminSearch from '../../../components/Common/Search/AdminSearch';
+
 function UserAdmin() {
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
+  const handleSearch = (searchColumn, searchTerm) => {
+    const filteredData = users.filter((user) => {
+      if (!searchColumn) {
+        return user.id.toString().toLowerCase().includes(searchTerm.toLowerCase());
+      }
+      return user[searchColumn].toString().toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    setFilteredUsers(filteredData);
+  };
+
+  const getAllUsers = async () => {
+    const usersList = await fetchData('GET', 'admin/users', null, true);
+    setFilteredUsers(usersList);
+    setUsers(usersList);
+  };
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
   return (
-    <h1>User</h1>
+    <>
+      <h1>User</h1>
+      <AdminSearch datas={users} onSearch={handleSearch} />
+
+      <AdminTable datas={filteredUsers} />
+
+    </>
   );
 }
 
