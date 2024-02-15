fb774b207289b91fc5ab0f7202914f56baca17e4
MerylCordier <meryl.cordier@oclock.school>
2024-02-15 23:26:40 +0100

    styles(global): color navbar and label page

--- src/pages/Label/index.jsx
+++ src/pages/Label/index.jsx
@@ -1,16 +1,20 @@
 /* eslint-disable react/prop-types */
-// import './index.css'
+import './index.css';
 
 function Label({ labelsWhithAlbums }) {
   return (
     labelsWhithAlbums.map((label) => (
-      <div className="label-name" key={label.id}>
-        {label.name}
-        {label.albums.map((album) => (
-          <div className="album" key={album.id}>
-            <img className="album-image" src={album.url_image} alt={album.name} />
-          </div>
-        ))}
+      <div className="label-container">
+        <div className="label-name hero" key={label.id}>
+          {label.name}
+        </div>
+        <div className="label-albums">
+          {label.albums.map((album) => (
+            <div className="album" key={album.id}>
+              <img className="album-image" src={album.url_image} alt={album.name} />
+            </div>
+          ))}
+        </div>
       </div>
     ))
   );
