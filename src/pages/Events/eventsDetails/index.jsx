7c1b25742235066311daa19ddc2cc301c80b1a96
kevinstandaert <kevin.standaert@oclock.school>
2024-02-15 16:39:29 +0100

    feat partial dynamization

---
+++ src/pages/Events/eventsDetails/index.jsx
@@ -0,0 +1,53 @@
+/* eslint-disable react/prop-types */
+function EventsDetails({ eventsDetails }) {
+  //   const [uneVariable, setUneVariable] = useState('');
+  const events = eventsDetails.filter((event) => event.name);
+
+  return (
+    <>
+      <h1 className="event_h1">
+        Evénements
+      </h1>
+
+      {
+
+            events.map((event) => (
+              <div className="event_container" key={event.id}>
+                <div className="img_container">
+                  <img className="event_image" src="images/Barracuda.jpg" alt="Barracuda" />
+                </div>
+                <div className="text_containeur">
+                  <h2 className="event_h2">
+                    {event.name}
+                  </h2>
+                  <p className="event_type global_description">
+                    {event.type}
+                  </p>
+                  <p className="event_description global_description">
+                    {event.description}
+                  </p>
+                  <p className="event_date_debut global_description">
+                    {event.starting_date}
+                  </p>
+                  <p className="event_date_fin global_description">
+                    {event.ending_date}
+                  </p>
+                  <p className="event_ville global_description">
+                    {event.city}
+                  </p>
+                  <p className="event_pays global_description">
+                    {event.country}
+                  </p>
+                  <p className="event_lieu global_description">
+                    {event.location}
+                  </p>
+                </div>
+              </div>
+            ))
+        }
+
+    </>
+  );
+}
+
+export default EventsDetails;
--- src/pages/Events/index.jsx
+++ src/pages/Events/index.jsx
@@ -1,86 +1,30 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable max-len */
 import './index.css';
 
 import { useState, useEffect } from 'react';
 
+import EventsDetails from './eventsDetails';
+
 function Events() {
 //   const [uneVariable, setUneVariable] = useState('');
+  const [eventsDetails, setEventsDetails] = useState([]);
+
+  const fetchDetails = async () => {
+    try {
+      const response = await fetch('http://localhost:4000/api/labels');
+      const data = await response.json();
+      setEventsDetails(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchDetails();
+  }, []);
 
   return (
-    <div>
-      <h1 className="event_h1">
-        Evénements
-      </h1>
-      <div className="global">
-
-        <div className="event_container">
-          <div className="img_container">
-            <img className="event_image" src="images/Barracuda.jpg" alt="Barracuda" />
-          </div>
-          <div className="text_containeur">
-            <h2 className="event_h2">
-              Festival de Dour
-            </h2>
-            <p className="event_type global_description">
-              Festival
-            </p>
-            <p className="event_description global_description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam neque ut, explicabo similique praesentium tempora exercitationem nesciunt assumenda, cum libero voluptatum quisquam deserunt fuga, pariatur ducimus rem omnis accusamus? Aliquam?
-            </p>
-            <p className="event_date_debut global_description">
-              Lundi 12 février 2024 à 9h00
-            </p>
-            <p className="event_date_fin global_description">
-              Marcredi 14 février 2024 à 23h00
-            </p>
-            <p className="event_ville global_description">
-              Paris
-            </p>
-            <p className="event_pays global_description">
-              France
-            </p>
-            <p className="event_lieu global_description">
-              Scène 28
-            </p>
-          </div>
-        </div>
-
-        <div className="event_container">
-          <div className="img_container">
-            <img className="event_image" src="images/fete_zik_2014.jpg" alt="fete_zik_2014" />
-          </div>
-          <div className="text_containeur">
-            <h2 className="event_h2">
-              Festival de Dour
-            </h2>
-            <p className="event_type global_description">
-              Festival
-            </p>
-            <p className="event_description global_description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam neque ut, explicabo similique praesentium tempora exercitationem nesciunt assumenda, cum libero voluptatum quisquam deserunt fuga, pariatur ducimus rem omnis accusamus? Aliquam?
-            </p>
-            <p className="event_date_debut global_description">
-              Lundi 12 février 2024 à 9h00
-            </p>
-            <p className="event_date_fin global_description">
-              Marcredi 14 février 2024 à 23h00
-            </p>
-            <p className="event_ville global_description">
-              Paris
-            </p>
-            <p className="event_pays global_description">
-              France
-            </p>
-            <p className="event_lieu global_description">
-              Scène 28
-            </p>
-          </div>
-        </div>
-
-      </div>
-
-    </div>
-
+    <EventsDetails eventsDetails={eventsDetails} />
   );
 }
 
