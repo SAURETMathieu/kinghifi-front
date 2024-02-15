d4abc84178f7a37f1d8d4303704e6b6a14d589d6
SAURETMathieu <mathieu.sauret@oclock.school>
2024-02-15 09:24:30 +0100

    feat(adminPage): create a guard to redirect the user if he isn't admin

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,5 +1,26 @@
-// import './index.css'
+import { useEffect, useContext, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+import checkAdminRole from '../../services/auth/checkAdmin';
 
-const Admin = () => ('Admin');
+import { UserContext } from '../../context/userContext';
+
+function Admin() {
+  const { isAdmin, setIsAdmin } = useContext(UserContext);
+  const [isAdminChecked, setIsAdminChecked] = useState(false);
+
+  useEffect(() => {
+    const adminRole = checkAdminRole();
+    setIsAdmin(adminRole);
+    setIsAdminChecked(true);
+  }, []);
+
+  if (isAdminChecked && !isAdmin) {
+    return <Navigate to="/" replace />;
+  }
+  if (isAdminChecked && isAdmin) {
+    return 'Admin';
+  }
+  return null;
+}
 
 export default Admin;
