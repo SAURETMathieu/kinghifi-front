9ebc2eaffdfe8410122fb22b8d3a2cf7b3310842
kevinstandaert <kevin.standaert@oclock.school>
2024-02-15 15:33:21 +0100

    feat event component responsive but  NON DYNAMIC WITH DB

--- src/pages/Events/index.jsx
+++ src/pages/Events/index.jsx
@@ -1,17 +1,86 @@
-// import './index.css'
+/* eslint-disable max-len */
+import './index.css';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Events() {
-  const [uneVariable, setUneVariable] = useState('');
+//   const [uneVariable, setUneVariable] = useState('');
 
   return (
     <div>
-      Les évènements
-      <div>
-        {uneVariable}
+      <h1 className="event_h1">
+        Evénements
+      </h1>
+      <div className="global">
+
+        <div className="event_container">
+          <div className="img_container">
+            <img className="event_image" src="images/Barracuda.jpg" alt="Barracuda" />
+          </div>
+          <div className="text_containeur">
+            <h2 className="event_h2">
+              Festival de Dour
+            </h2>
+            <p className="event_type global_description">
+              Festival
+            </p>
+            <p className="event_description global_description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam neque ut, explicabo similique praesentium tempora exercitationem nesciunt assumenda, cum libero voluptatum quisquam deserunt fuga, pariatur ducimus rem omnis accusamus? Aliquam?
+            </p>
+            <p className="event_date_debut global_description">
+              Lundi 12 février 2024 à 9h00
+            </p>
+            <p className="event_date_fin global_description">
+              Marcredi 14 février 2024 à 23h00
+            </p>
+            <p className="event_ville global_description">
+              Paris
+            </p>
+            <p className="event_pays global_description">
+              France
+            </p>
+            <p className="event_lieu global_description">
+              Scène 28
+            </p>
+          </div>
+        </div>
+
+        <div className="event_container">
+          <div className="img_container">
+            <img className="event_image" src="images/fete_zik_2014.jpg" alt="fete_zik_2014" />
+          </div>
+          <div className="text_containeur">
+            <h2 className="event_h2">
+              Festival de Dour
+            </h2>
+            <p className="event_type global_description">
+              Festival
+            </p>
+            <p className="event_description global_description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam neque ut, explicabo similique praesentium tempora exercitationem nesciunt assumenda, cum libero voluptatum quisquam deserunt fuga, pariatur ducimus rem omnis accusamus? Aliquam?
+            </p>
+            <p className="event_date_debut global_description">
+              Lundi 12 février 2024 à 9h00
+            </p>
+            <p className="event_date_fin global_description">
+              Marcredi 14 février 2024 à 23h00
+            </p>
+            <p className="event_ville global_description">
+              Paris
+            </p>
+            <p className="event_pays global_description">
+              France
+            </p>
+            <p className="event_lieu global_description">
+              Scène 28
+            </p>
+          </div>
+        </div>
+
       </div>
+
     </div>
+
   );
 }
 
