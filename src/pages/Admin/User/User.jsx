3cbec2e8df85942ecb3835fb75896966704dc196
SAURETMathieu <mathieu.sauret@oclock.school>
2024-02-16 13:52:04 +0100

    feat(User): dynamise user admin component

--- src/pages/Admin/User/User.jsx
+++ src/pages/Admin/User/User.jsx
@@ -1,40 +1,8 @@
-import { useState, useEffect } from 'react';
-import fetchData from '../../../services/api/call.api';
-import AdminTable from '../../../components/Common/Table/AdminTable';
-import AdminSearch from '../../../components/Common/Search/AdminSearch';
+import AdminTemplate from '../Template/Template';
 
 function UserAdmin() {
-  const [users, setUsers] = useState([]);
-  const [filteredUsers, setFilteredUsers] = useState([]);
-
-  const handleSearch = (searchColumn, searchTerm) => {
-    const filteredData = users.filter((user) => {
-      if (!searchColumn) {
-        return user.id.toString().toLowerCase().includes(searchTerm.toLowerCase());
-      }
-      return user[searchColumn].toString().toLowerCase().includes(searchTerm.toLowerCase());
-    });
-    setFilteredUsers(filteredData);
-  };
-
-  const getAllUsers = async () => {
-    const usersList = await fetchData('GET', 'admin/users', null, true);
-    setFilteredUsers(usersList);
-    setUsers(usersList);
-  };
-
-  useEffect(() => {
-    getAllUsers();
-  }, []);
-
   return (
-    <>
-      <h1>Users</h1>
-      <AdminSearch datas={users} onSearch={handleSearch} />
-
-      <AdminTable datas={filteredUsers} />
-
-    </>
+    <AdminTemplate route="admin/users" title="Utilisateurs" />
   );
 }
 
