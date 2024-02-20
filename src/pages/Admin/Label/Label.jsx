b6256f7148588af5857d00fa9f33d72a8cde387b
SAURETMathieu <mathieu.sauret@oclock.school>
2024-02-20 09:12:47 +0100

    feat(admin pages): add create form

--- src/pages/Admin/Label/Label.jsx
+++ src/pages/Admin/Label/Label.jsx
@@ -1,8 +1,18 @@
 import AdminTemplate from '../Template/Template';
+import { options } from '../../../data/formElement.json';
 
 function LabelAdmin() {
+  const optionsList = [
+    options.name,
+    options.year,
+    options.city,
+    options.country,
+    options.description,
+    options.url_image,
+  ];
+
   return (
-    <AdminTemplate route="admin/labels" title="Labels" />
+    <AdminTemplate route="admin/labels" title="Labels" optionsList={optionsList} />
   );
 }
 
