4dbe7947fef25a59fe5ed5a8d67da5eac4b23d66
kevinstandaert <kevin.standaert@oclock.school>
2024-02-20 10:18:35 +0100

    add comment and fix css for images

--- src/pages/Medias/mediasDetails/index.jsx
+++ src/pages/Medias/mediasDetails/index.jsx
@@ -1,21 +1,9 @@
 /* eslint-disable react/prop-types */
-import { NavLink, Outlet } from 'react-router-dom';
 
 function MediasDetails({ mediasDetails }) {
   const medias = mediasDetails.filter((media) => media.id);
 
   console.log(medias);
-  // const filteredName = medias.map((media) => (
-  //   media.socials.map((social) => (
-  //     social.url.split('https://facebook.com/')
-  //   ))
-  // ));
-
-  // const crewNameFiltered = filteredName.map((element) => (
-  //   element[0].pop()
-  // ));
-
-  // console.log(crewNameFiltered);
 
   return (
     <>
@@ -23,16 +11,23 @@ function MediasDetails({ mediasDetails }) {
         Médias
       </h1>
 
+      {/* Implementation of the Facebook group page. Awaiting validation by Meta */}
+
+      {/* <div id="fb-root" />
+      <script async defer crossOrigin="anonymous" src="https://connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v19.0&appId=1783266342176460" nonce="WsCVWgUP" />
+
+      <div className="fb-page" data-href="https://www.facebook.com/pure.niceness.records" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/pure.niceness.records" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/pure.niceness.records">Pure Niceness Records</a></blockquote></div>
+
       <div className="fb_containeur">
         <iframe className="fb" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fpure.niceness.records&tabs=timeline&width=500&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" title="facebook" width="500" height="500" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" />
 
         <iframe className="fb" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fmentalstamina&tabs=timeline&width=500&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" title="facebook3" width="500" height="500" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" />
 
         <iframe className="fb" src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fking.hi.fi&tabs=timeline&width=500&height=500&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" title="facebook2" width="500" height="500" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" />
-      </div>
+      </div> */}
 
       <div className="video_up_container">
-        <iframe className="video_up" width="560" height="315" src="https://www.youtube.com/embed/aUN5L2zJwlU?si=OqZJxV8nOvUHm6n9" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen />
+        <iframe className="video_up" width="560" height="315" src="https://www.youtube.com/embed/aUN5L2zJwlU?si=OqZJxV8nOvUHm6n9" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen />
       </div>
 
       <div className="images_container">
