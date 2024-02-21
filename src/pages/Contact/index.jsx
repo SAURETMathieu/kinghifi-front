58ed4c78c9b7e045e42c7ecb2baf81a39a87a9ab
kevinstandaert <kevin.standaert@oclock.school>
2024-02-21 14:00:45 +0100

    feat contact form no css no server connection

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,7 +1,67 @@
-// import './index.css'
+import './index.css';
+import { useState } from 'react';
 
-function Contact() {
-  return ('Contact');
+function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    objet: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData); // A changer pour envoyer au serveur
+
+    setFormData({
+      name: '',
+      email: '',
+      objet: '',
+      message: '',
+    });
+  };
+
+  return (
+    <>
+      <h1 className="contact-h1">
+        Contact
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">
+            Nom:
+            <input type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="email">
+            Email:
+            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="objet">
+            Objet:
+            <input type="text" id="objet" name="objet" value={formData.objet} onChange={handleChange} />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="message">
+            Message:
+            <textarea id="message" name="message" value={formData.message} onChange={handleChange} />
+          </label>
+        </div>
+        <button type="submit">Envoyer</button>
+      </form>
+    </>
+  );
 }
 
-export default Contact;
+export default ContactForm;
