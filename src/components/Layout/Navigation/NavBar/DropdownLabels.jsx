855a2e85e84e8e8f6e6ab17342f697e07768cd1a
MerylCordier <meryl.cordier@oclock.school>
2024-02-21 15:00:20 +0100

    fix(loader): loader on music page nead fix(dropdown): link name music page

--- src/components/Layout/Navigation/NavBar/DropdownLabels.jsx
+++ src/components/Layout/Navigation/NavBar/DropdownLabels.jsx
@@ -1,13 +1,18 @@
 import { faMusic } from '@fortawesome/free-solid-svg-icons';
-
+import fetchData from '../../../../services/api/call.api';
 import Dropdown from '../../../Common/Dropdown';
 
+const labelsData = await fetchData('GET', 'labels');
+
 function DropdownLabel() {
+  const labelName = labelsData.map((label) => label.name);
+
   const labelLinks = [
-    { id: 1, path: '/labels/1', label: 'les sons du label 1' },
-    { id: 2, path: '/labels/2', label: 'les sons du label 2' },
-    { id: 3, path: '/labels/3', label: 'les sons du label 3' },
-    { id: 4, path: '/labels', label: 'tous nos sons' },
+    { id: 1, path: '/labels/1', label: labelName[0] },
+    { id: 2, path: '/labels/2', label: labelName[1] },
+    { id: 3, path: '/labels/3', label: labelName[2] },
+    // lien vers toutes les musiques: page non fonctionnelle
+    // { id: 4, path: '/labels', label: 'all Records' },
 
   ];
 
