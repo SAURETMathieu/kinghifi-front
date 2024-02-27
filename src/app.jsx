1d6ea5142def8dcd974266b7618535acce9afa45
SAURETMathieu <mathieu.sauret@oclock.school>
2024-02-27 10:03:29 +0100

    style(toastify): change style and duration of toast

--- src/app.jsx
+++ src/app.jsx
@@ -15,7 +15,7 @@ function App() {
 
   return (
     <div className="app">
-      <ToastContainer />
+      <ToastContainer autoClose={1800} theme="dark" />
       <Header />
       <main>
         <Outlet />
