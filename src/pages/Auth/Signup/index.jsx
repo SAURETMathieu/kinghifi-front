bc9d93bdd29b344275d0a84b895e296f8cafcf25
SAURETMathieu <mathieu.sauret@oclock.school>
2024-03-25 17:20:12 +0100

    style(signup): responsive signup form

--- src/pages/Auth/Signup/index.jsx
+++ src/pages/Auth/Signup/index.jsx
@@ -63,7 +63,7 @@ function Singup() {
       <h1 className="contact-h1">
         Inscription
       </h1>
-      <form className="form_containeur" onSubmit={handleSubmit}>
+      <form className="form__signup-container" onSubmit={handleSubmit}>
 
         <div className="form_div">
           <label className="form_label" htmlFor="email">
@@ -130,7 +130,7 @@ function Singup() {
 
         <div className="form_div">
           <label className="form_label" htmlFor="passwordConfirm">
-            <p className="label_name">Confirmez votre mot de passe</p>
+            <p className="label_name">Confirmation</p>
             <input className="contact_input" type="password" id="passwordConfirm" name="passwordConfirm" value={formUserData.passwordConfirm} onChange={handleChange} />
           </label>
         </div>
